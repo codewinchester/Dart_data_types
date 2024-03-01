183e744d97e3ecb2f86b49dba8eef78bba5d0a4d
codewinchester <160033340+codewinchester@users.noreply.github.com>
2024-03-01 20:57:17 +0300

    Add files via upload

---
+++ data_types.dart
@@ -0,0 +1,101 @@
+//void main() {
+  // Integers
+  int age = 25;
+  
+  // Doubles which i usually compare to floats in python just for understanding.
+  double weight = 65.5;
+  
+  // Strings
+  String name = "John Doe";
+  
+  // Lists
+  List<String> hobbies = ["Reading", "Gaming", "Cooking"];
+  List<int> numbers = [30, 40, 50];
+  
+  // Maps comparable to dictionaries in python
+  Map<String, dynamic> person = {
+    'name': 'Alice',
+    'age': 30,
+    'isStudent': true,
+    'grades': {'math': 90, 'science': 85}
+  };
+  
+  // Printing out the data
+  //print("Age: $age");
+  //print("Weight: $weight");
+  //print("Name: $name");
+  //print("Hobbies: $hobbies");
+  //print("Numbers: $numbers");
+  //print("Person: $person");
+
+
+//This is a function that does addition
+//void addTwo(int num1, int num2){
+  //int sum = num1 + num2;
+  //print("The sum is $sum");
+//}
+
+//void main(){
+  //addTwo(10, 20);
+//}
+
+
+//This is a function that does subtarction
+//void SubtractTwo (int num1, int num2){
+  //int sub = num2 - num1;
+  //print("The difference is $sub");
+//}
+  //void main(){
+  //SubtractTwo(10, 20);
+//}
+
+//This is a function that multiplies two numbers.
+//void multiplyTwo (int num1, int num2){
+  //int mult = num1 * num2;
+  //print("The multiplication of the two numbers is $mult");
+//}
+  //void main(){
+    //multiplyTwo(10, 20);
+  //}
+
+//A function that outputs the remainder of a division of two numbers
+//void divideTwo (int num1, int num2){
+  //int div = num1 % num2; 
+  //print ("The quotient of the two numbers is $div");
+  //}
+
+  //void main(){
+    //divideTwo(80, 80);
+  //}
+
+//A function that outputs a string's length
+//main() { 
+    
+  // Declaring a string variable 
+  //String name = "Kenneth Kipchumba"; 
+    
+  // Finding the length 
+  //int length = name.length; 
+    
+  // Printing the length of the string 
+  //print(length); 
+//}
+
+//dynamic getFirstElement(List list) {
+  //if (list.isEmpty) {
+    //return null; // Return null if the list is empty
+  //} else {
+    //return list[0]; // Return the first element of the list
+  //}
+//}
+
+//void main() {
+  //List<int> numbers = [1, 2, 3, 4, 5];
+  //print(getFirstElement(numbers));
+//}
+
+
+
+
+
+
